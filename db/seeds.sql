f11fd9fecb35f6ba4a59d477a76cdccc1b5e9274
mighty-little-coder <144641610+mighty-little-coder@users.noreply.github.com>
2023-12-07 01:49:51 -0500

    Added department and roles table info

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,11 +2,21 @@ INSERT INTO department (name)
 VALUES  ("Engineering"),
         ("Sales"),
         ("Assembly"),
-        ("Finance"),
         ("Service");
 
 INSERT INTO role (title, salary, department_id)
 VALUES  ("Mechanical Engineer", 80000, 1),
-        ("Electrical Engineer", 95000, 1);
+        ("Electrical Engineer", 95000, 1),
+        ("Test Engineering", 90000, 1),
+        ("Government Contracts", 120000, 2),
+        ("Private Contracts", 110000, 2),
+        ("Hydraulic Assembly", 55000, 3),
+        ("Electrical Assembly", 55000, 3),
+        ("Customer Service", 80000, 4),
+        ("Financial Planning", 80000, 4),
+        ("Service Technician", 75000, 4);
+
+
+        
 
 
