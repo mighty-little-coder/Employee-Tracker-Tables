27dc9daeecd36b990bc1610181db13c207cb2477
mighty-little-coder <144641610+mighty-little-coder@users.noreply.github.com>
2023-12-07 01:11:09 -0500

    Fixed schema.sql and added base to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (name)
+VALUES  ("Engineering"),
+        ("Sales"),
+        ("Assembly"),
+        ("Finance"),
+        ("Service");
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Mechanical Engineer", 80000, 1),
+        ("Electrical Engineer", 95000, 1);
+
+
