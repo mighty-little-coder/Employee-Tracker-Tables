bb8e37765d6846600ffd01ee9bf57ac0843c458c
mighty-little-coder <144641610+mighty-little-coder@users.noreply.github.com>
2023-12-12 18:04:47 -0500

    Changed server to use promises to complete functions

--- server2.js
+++ server2.js
@@ -14,7 +14,7 @@ const db = mysql.createConnection(
     database: 'employeeTracker_db'
   },
   console.log(`Connected to the Employee Structure database.`)
-);
+).promise();
 
 // Start menu
 const start = [
@@ -100,12 +100,8 @@ function initialPrompt() {
 // Response functions according to initial prompt
 async function viewEmp() {
   try {
-    db.query ('SELECT * FROM employee', function (err, results) {
-        if (err) {
-          throw err;
-        }
+    const results = await db.query ('SELECT * FROM employee')
         console.table(results)
-    })
   } catch (error) {
     console.error(error);
   }
@@ -213,40 +209,15 @@ async function getRoles() {
 // };
 
 async function viewDep() {
-  try {
-    db.query ('SELECT * FROM department', function (err, results) {
-        if (err) {
-          throw err;
-        }
-        console.table(results)
-    })
-  } catch (error) {
-    console.error(error);
-  }
-  initialPrompt();
-};
+    try {
+      const results = await db.query ('SELECT * FROM department')
+          console.table(results[0])
+    } catch (error) {
+      console.error(error);
+    }
+    initialPrompt();
+  };
 
-// async function addDep() {
-//   try {
-//   function addDep() {
-//     inquirer
-//         .prompt({
-//             type: "input",
-//             name: "newDepName",
-//             message: "Enter the title of the new department:",
-//         })
-//         .then((answer) => {
-//             console.log(answer.name);
-//             const query = `INSERT INTO department (department_name) VALUES ("${answer.name}")`;
-//             connection.query(query, (err, res) => {
-//                 if (err) throw err;
-//                 console.log(`Added department ${answer.name} to the database!`);
-//                 // restart the application
-//                 start();
-//                 console.log(answer.name);
-//             });
-//         });
-// }
 //--------------------------------------------------------------------------------
 async function addDep() {
   try {
@@ -264,11 +235,8 @@ async function addDep() {
   }
   initialPrompt();
 };
-//--------------------------------------------------------------------------------
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
+//-------------------------------------------------------------------------------
+
 
 // async function delDep() {
 //   try {
@@ -312,122 +280,3 @@ function quit() {
 
 
 initialPrompt();
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-// NEEDS ADJUSTED CODE FOR CRUD PROGRAMS. BELOW FOR REFERENCE
-
-
-
-// // Create a movie
-// app.post('/api/new-movie', ({ body }, res) => {
-//   const sql = `INSERT INTO movies (movie_name)
-//     VALUES (?)`;
-//   const params = [body.movie_name];
-  
-//   db.query(sql, params, (err, result) => {
-//     if (err) {
-//       res.status(400).json({ error: err.message });
-//       return;
-//     }
-//     res.json({
-//       message: 'success',
-//       data: body
-//     });
-//   });
-// });
-
-// // Read all movies
-// app.get('/api/movies', (req, res) => {
-//   const sql = `SELECT id, movie_name AS title FROM movies`;
-  
-//   db.query(sql, (err, rows) => {
-//     if (err) {
-//       res.status(500).json({ error: err.message });
-//        return;
-//     }
-//     res.json({
-//       message: 'success',
-//       data: rows
-//     });
-//   });
-// });
-
-// // Delete a movie
-// app.delete('/api/movie/:id', (req, res) => {
-//   const sql = `DELETE FROM movies WHERE id = ?`;
-//   const params = [req.params.id];
-  
-//   db.query(sql, params, (err, result) => {
-//     if (err) {
-//       res.statusMessage(400).json({ error: res.message });
-//     } else if (!result.affectedRows) {
-//       res.json({
-//       message: 'Movie not found'
-//       });
-//     } else {
-//       res.json({
-//         message: 'deleted',
-//         changes: result.affectedRows,
-//         id: req.params.id
-//       });
-//     }
-//   });
-// });
-
-// // Read list of all reviews and associated movie name using LEFT JOIN
-// app.get('/api/movie-reviews', (req, res) => {
-//   const sql = `SELECT movies.movie_name AS movie, reviews.review FROM reviews LEFT JOIN movies ON reviews.movie_id = movies.id ORDER BY movies.movie_name;`;
-//   db.query(sql, (err, rows) => {
-//     if (err) {
-//       res.status(500).json({ error: err.message });
-//       return;
-//     }
-//     res.json({
-//       message: 'success',
-//       data: rows
-//     });
-//   });
-// });
-
-// // BONUS: Update review name
-// app.put('/api/review/:id', (req, res) => {
-//   const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
-//   const params = [req.body.review, req.params.id];
-
-//   db.query(sql, params, (err, result) => {
-//     if (err) {
-//       res.status(400).json({ error: err.message });
-//     } else if (!result.affectedRows) {
-//       res.json({
-//         message: 'Movie not found'
-//       });
-//     } else {
-//       res.json({
-//         message: 'success',
-//         data: req.body,
-//         changes: result.affectedRows
-//       });
-//     }
-//   });
-// });
-
-// // Default response for any other request (Not Found)
-// app.use((req, res) => {
-//   res.status(404).end();
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
